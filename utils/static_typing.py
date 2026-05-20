5326e175e7a57fd78fac6d9b81a70d289d3c723a
Daynlight <danielstodulski000@gmail.com>
2026-05-20 09:30:13 +0100

    Executable and Python tests workflow works

--- utils/static_typing.py
+++ utils/static_typing.py
@@ -1,8 +1,7 @@
 import sys
 
 
-# if not getattr(sys, "frozen", False):
-#     from typeguard import typechecked
-# else:
-#     def typechecked(x): return x
-from typeguard import typechecked
+if not getattr(sys, "frozen", False):
+    from typeguard import typechecked
+else:
+    def typechecked(x): return x
