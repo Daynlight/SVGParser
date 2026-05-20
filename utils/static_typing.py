00dab08c97ecb00b95cad288cfe1142f6060d00f
Daynlight <danielstodulski000@gmail.com>
2026-05-20 09:29:04 +0100

    Executable fail build

--- utils/static_typing.py
+++ utils/static_typing.py
@@ -1,7 +1,8 @@
 import sys
 
 
-if not getattr(sys, "frozen", False):
-    from typeguard import typechecked
-else:
-    def typechecked(x): return x
+# if not getattr(sys, "frozen", False):
+#     from typeguard import typechecked
+# else:
+#     def typechecked(x): return x
+from typeguard import typechecked
