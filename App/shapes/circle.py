68e55d5482d4c33eb2cb5e42d243d5d8bc1c651a
Daynlight <danielstodulski000@gmail.com>
2026-05-28 18:11:01 +0100

    Basic Parsing from file, last_time_write, Circle validation, Circle parsing, parser uses subclasses

--- App/shapes/circle.py
+++ App/shapes/circle.py
@@ -1,5 +1,7 @@
 from utils.static_typing import typechecked
+from rich import print
 import arcade
+import re
 
 from App.shapes.shape import Shape
 
@@ -8,7 +10,8 @@ from App.shapes.shape import Shape
 @typechecked
 class Circle(Shape):
   @typechecked
-  def __init__(self, position: tuple[int, int], radius: int) -> None:
+  def __init__(self, position: tuple[int, int] = (0, 0), radius: int = 0) -> None:
+    super().__init__()
     self._position: tuple[int, int] = position
     self._radius: int = radius
     self._color: arcade.color = arcade.color.AZURE
@@ -20,5 +23,21 @@ class Circle(Shape):
 
 
   @typechecked
-  def parse(self, data: str) -> None:
-    pass
+  def parse(self, data: str, entry: int) -> None:
+    pairs = dict(re.findall(r"([xyr])\s*=\s*([+-]?\d+)", data))
+  
+    required_keys = {'x', 'y', 'r'}
+    if not required_keys.issubset(pairs.keys()):
+        missing = required_keys - pairs.keys()
+        print(f"[red]On entry {entry}: Missing parameters {missing} in: {data}[/red]")
+        return
+
+    self._position = (int(pairs['x']), int(pairs['y']))
+    self._radius = int(pairs['r'])
+
+
+  @typechecked
+  @classmethod
+  def validate(self, data: str) -> bool:
+    pattern: str = r"Circle"
+    return bool(re.search(pattern, data))
--- App/shapes/shape.py
+++ App/shapes/shape.py
@@ -5,6 +5,11 @@ from utils.static_typing import typechecked
 
 @typechecked
 class Shape(ABC):
+  @classmethod
+  def getAllShapes(cls):
+    return cls.__subclasses__()
+  
+  
   @typechecked
   def __init__(self) -> None:
     pass
@@ -18,5 +23,12 @@ class Shape(ABC):
 
   @typechecked
   @abstractmethod
-  def parse(self, data: str) -> None:
+  def parse(self, data: str, entry: int) -> None:
     pass
+
+
+  @typechecked
+  @classmethod
+  @abstractmethod
+  def validate(self, data: str) -> bool:
+    return False
