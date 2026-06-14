0fa817988b956c2cbf6ca22e3b6c6f6c6e82a2d3
Martyna <marple6@st.amu.edu.pl>
2026-06-14 13:35:06 +0200

    Added colors to the rectnagle class, but colors needs to be added to the rest and to the test unit. Currently it is for tests

--- App/shapes/rectangle.py
+++ App/shapes/rectangle.py
@@ -4,6 +4,7 @@ import arcade
 import re
 
 from App.shapes.shape import Shape
+from App.shapes.colors import ColorParser
 
 @typechecked
 class Rectangle(Shape):
@@ -25,26 +26,36 @@ class Rectangle(Shape):
     def parse(self, data: str, entry: int) -> bool:
         data = re.sub(r"^[Rr]ectangle\s*\(\s*", "", data)
         data = re.sub(r"\)\s*;?\s*$", "", data)
-        pattern = r"^\s*(?:[xywh]\s*=\s*[+-]?\d+(?:\.\d+)?\s*,\s*)*[xywh]\s*=\s*[+-]?\d+(?:\.\d+)?\s*;?\s*$"
+        pattern = r'^\s*(?:(?:[xywhc]|color)\s*=\s*[\w#\'".+-]+\s*,\s*)*(?:[xywhc]|color)\s*=\s*[\w#\'".+-]+\s*;?\s*$'
 
         if not re.fullmatch(pattern, data, re.VERBOSE):
             print(f"[yellow]On entry {entry}: invalid format[/yellow] [blue]{data}[/blue]")
             return False
     
-        pairs = dict(re.findall(r"([xywh])\s*=\s*([+-]?\d+(?:\.\d+)?)", data))
+        pairs = dict(re.findall(r'([xywhc]|color)\s*=\s*([\w#\'".+-]+)', data))
 
         required_keys = {'x', 'y', 'w', 'h'}
+        allowed_keys = {'x', 'y', 'w', 'h', 'c', 'color'}
 
         if not required_keys.issubset(pairs.keys()):
 
             missing = required_keys - pairs.keys()
-
             print(f"[yellow]On entry {entry}: Missing parameters [/yellow][blue]{missing}[/blue][yellow] in: {data}[/yellow]")
+            return False
+        
+        invalid_keys = set(pairs.keys()) - allowed_keys
+        if invalid_keys:
+            print(f"[yellow]On entry {entry}: Unrecognized parameters [/yellow][red]{invalid_keys}[/red][yellow] in: {data}[/yellow]")
             return False
         
         self._position = (float(pairs['x']), float(pairs['y']))
         self._width = float(pairs['w'])
         self._height = float(pairs['h'])
+
+        if 'color' in pairs:
+            self._color = ColorParser.parse(pairs['color'])
+        elif 'c' in pairs:
+            self._color = ColorParser.parse(pairs['c'])
 
         return True
     
